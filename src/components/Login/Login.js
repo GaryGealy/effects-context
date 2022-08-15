9679c1820f11d1c204a7d72de696f449d2919a41
Gary Gealy <GaryGealy@users.noreply.github.com>
2022-08-15 17:47:48 -0500

    added cleanup function to userEffect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,10 +12,18 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
-  }, [enterEmail, enteredPassword]);
+    const indentifier = setTimeout(() => {
+      console.log('Validity check');
+      setFormIsValid(
+          enteredEmail.includes("@") && enteredPassword.trim().length > 6
+        );
+    }, 500);
+
+    return () => {
+      console.log("CleanUp");
+      clearTimeout(indentifier);
+    };
+  }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
