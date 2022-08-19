45cd36dd3e4099770dbd437d167321b814605a8b
Gary Gealy <GaryGealy@users.noreply.github.com>
2022-08-18 20:44:10 -0500

    added Reducer

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,41 +1,59 @@
-import React, { useState, useEffect, useInsertionEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 
+const emailReducer = (state, action) => {
+  if ( action.type === 'USER_INPUT') {
+    return { value: action.val, isValid: action.val.includes('@') };
+  }
+
+  if ( action.type === 'INPUT_BLUR') {
+    return { value: state.value, isValid: state.value.includes('@')};
+  }
+  return { value: "", isValid: false };
+};
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [emailIsValid, setEmailIsValid] = useState();
+  //const [enteredEmail, setEnteredEmail] = useState("");
+  //const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  useEffect( () => {
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: false,
+  });
 
-  });  
+  const dispatchEmailHandler = () => {};
 
-  useEffect(() => {
-    // this will get called on every keystroke in enteredEmail and 
-    // enteredPassword.  
-    
-    // creates a timer each keystroke
-    const indentifier = setTimeout(() => { 
-      setFormIsValid(
-          enteredEmail.includes("@") && enteredPassword.trim().length > 6
-        );
-    }, 500);
+  //useEffect(() => {
+  // this will get called on every keystroke in enteredEmail and
+  // enteredPassword.
 
-    // clean up for useEffect() in this case, we clear timer before
-    // it fires, so that it will actually only go off when the user
-    // pauses typing.
-    return () => {
-      clearTimeout(indentifier);
-    };
-  }, [enteredEmail, enteredPassword]);
+  // creates a timer each keystroke
+  // const indentifier = setTimeout(() => {
+  //   setFormIsValid(
+  //       enteredEmail.includes("@") && enteredPassword.trim().length > 6
+  //     );
+  // }, 500);
+
+  // clean up for useEffect() in this case, we clear timer before
+  // it fires, so that it will actually only go off when the user
+  // pauses typing.
+  //   return () => {
+  //     clearTimeout(indentifier);
+  //   };
+  // }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({type: 'USER_INPUT', val: event.target.value});
+
+    setFormIsValid(
+      emailState.isValid && event.target.value.trim().length > 6
+    );
   };
 
   const passwordChangeHandler = (event) => {
@@ -43,7 +61,7 @@ const Login = (props) => {
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes("@"));
+    dispatchEmail({type: 'INPUT_BLUR'});
   };
 
   const validatePasswordHandler = () => {
@@ -52,7 +70,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -60,14 +78,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ""
+            emailState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
